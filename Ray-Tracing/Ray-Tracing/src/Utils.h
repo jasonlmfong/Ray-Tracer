2768d6a80973d5a148cd8c187467d005fe0eb3f4
Jason Fong <jasonlmfong@gmail.com>
2023-03-24 07:58:17 -0400

    changes to util functions

--- Ray-Tracing/Ray-Tracing/src/Utils.h
+++ Ray-Tracing/Ray-Tracing/src/Utils.h
@@ -27,6 +27,17 @@ inline double degrees_to_radians(double degrees) {
     return degrees * pi / 180.0;
 }
 
+inline vec3 reflect(const vec3& v, const vec3& n) {
+    return v - 2 * dot(v, n) * n;
+}
+
+inline vec3 refract(const vec3& uv, const vec3& n, double etai_over_etat) {
+    auto cos_theta = fmin(dot(-uv, n), 1.0);
+    vec3 r_out_perp = etai_over_etat * (uv + cos_theta * n);
+    vec3 r_out_parallel = -sqrt(fabs(1.0 - r_out_perp.length_squared())) * n;
+    return r_out_perp + r_out_parallel;
+}
+
 inline double clamp(double x, double min, double max) {
     if (x < min) return min;
     if (x > max) return max;
--- Ray-Tracing/Ray-Tracing/src/vec3.h
+++ Ray-Tracing/Ray-Tracing/src/vec3.h
@@ -118,17 +118,17 @@ inline double randomDouble(double min, double max) {
     return min + (max - min) * randomDouble();
 }
 
-inline static vec3 random() {
+inline static vec3 randomVec3() {
     return vec3(randomDouble(), randomDouble(), randomDouble());
 }
 
-inline static vec3 random(double min, double max) {
+inline static vec3 randomVec3(double min, double max) {
     return vec3(randomDouble(min, max), randomDouble(min, max), randomDouble(min, max));
 }
 
 inline vec3 random_in_unit_sphere() {
     while (true) {
-        auto p = random(-1, 1);
+        auto p = randomVec3(-1, 1);
         if (p.length_squared() >= 1) continue;
         return p;
     }
@@ -144,15 +144,4 @@ inline vec3 random_in_unit_disk() {
         if (p.length_squared() >= 1) continue;
         return p;
     }
-}
-
-inline vec3 reflect(const vec3& v, const vec3& n) {
-    return v - 2 * dot(v, n) * n;
-}
-
-inline vec3 refract(const vec3& uv, const vec3& n, double etai_over_etat) {
-    auto cos_theta = fmin(dot(-uv, n), 1.0);
-    vec3 r_out_perp = etai_over_etat * (uv + cos_theta * n);
-    vec3 r_out_parallel = -sqrt(fabs(1.0 - r_out_perp.length_squared())) * n;
-    return r_out_perp + r_out_parallel;
 }
